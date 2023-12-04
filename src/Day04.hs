d64a2909ab90907e5e9bdb790d53b7bae5e478af
Nestor Fabian Villar <95443544+nesfvillar@users.noreply.github.com>
2023-12-04 06:25:04 +0000

    Implemented partial solution to day 4 part 1

---
+++ src/Day04.hs
@@ -0,0 +1,17 @@
+module Day04 where
+
+data Card = Card {winners, cards :: [Int]}
+
+winningCards :: Card -> [Int]
+winningCards card = filter (\c -> c `elem` winners card) $ cards card
+
+value :: Card -> Int
+value card =
+  let n = length (winningCards card)
+   in if n == 0 then 0 else 2 ^ (n - 1)
+
+part1 :: [Card] -> Int
+part1 = sum . map value
+
+part2 :: [Card] -> Int
+part2 = undefined
