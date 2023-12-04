b0cefed8759178903857f4a9ad56c0808ee57121
Nestor Fabian Villar <95443544+nesfvillar@users.noreply.github.com>
2023-12-04 06:32:27 +0000

    Use natural numbers where possible

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,16 +1,18 @@
 module Day04 where
 
+import Numeric.Natural (Natural)
+
 data Card = Card {winningNumbers, numbers :: [Int]}
 
 winners :: Card -> [Int]
 winners card = filter (\c -> c `elem` winningNumbers card) $ numbers card
 
-value :: Card -> Int
+value :: Card -> Natural
 value card =
   let n = length (winners card)
    in if n == 0 then 0 else 2 ^ (n - 1)
 
-part1 :: [Card] -> Int
+part1 :: [Card] -> Natural
 part1 = sum . map value
 
 part2 :: [Card] -> Int
