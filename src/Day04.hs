95f43631fdeccf3ca821932bef0345d124e6f13d
Nestor Fabian Villar <95443544+nesfvillar@users.noreply.github.com>
2023-12-05 05:31:22 +0000

    Implemented partially part 2

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,19 +1,22 @@
 module Day04 where
 
-import Numeric.Natural (Natural)
-
 data Card = Card {winningNumbers, numbers :: [Int]}
 
 winners :: Card -> [Int]
 winners card = filter (\c -> c `elem` winningNumbers card) $ numbers card
 
-value :: Card -> Natural
+value :: Card -> Int
 value card =
   let n = length (winners card)
    in if n == 0 then 0 else 2 ^ (n - 1)
 
-part1 :: [Card] -> Natural
+part1 :: [Card] -> Int
 part1 = sum . map value
 
 part2 :: [Card] -> Int
-part2 = undefined
+part2 cards = part2' 0 cards $ amountCards cards
+  where
+    part2' acc [] [] = acc
+    part2' acc (c : cs) (n : ns) = part2' (acc + value c * n) cs ns
+
+    amountCards cards = undefined $ replicate (length cards) 1
