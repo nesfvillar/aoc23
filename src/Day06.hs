c4d81017e30eb6385a5a3cbfbee24e0cc0dc3534
Nestor Fabian Villar <95443544+nesfvillar@users.noreply.github.com>
2023-12-07 23:59:12 +0000

    Use currying and don't create another input

--- src/Day06.hs
+++ src/Day06.hs
@@ -6,19 +6,21 @@ maxTimes = [59, 68, 82, 74]
 
 distances = [543, 1020, 1664, 1022]
 
+input :: [(Int, Int)]
 input = zip maxTimes distances
 
-waysToWin :: (Int, Int) -> Int
-waysToWin (t, d) = length $ filter (wins d t) [0 .. t]
+waysToWin :: Int -> Int -> Int
+waysToWin t d = length $ filter (wins d t) [0 .. t]
   where
     wins d mt t = d < t * (mt - t)
 
 part1 :: [(Int, Int)] -> Int
-part1 = product . map waysToWin
+part1 = product . map (uncurry waysToWin)
 
-input' = (parse maxTimes, parse distances)
+parse :: ([Int], [Int]) -> (Int, Int)
+parse (x, y) = (parse' x, parse' y)
   where
-    parse = (read :: String -> Int) . foldl1 (++) . map show
+    parse' = (read :: String -> Int) . foldl1 (++) . map show
 
-part2 :: (Int, Int) -> Int
-part2 = waysToWin
+part2 :: [(Int, Int)] -> Int
+part2 = uncurry waysToWin . parse . unzip
